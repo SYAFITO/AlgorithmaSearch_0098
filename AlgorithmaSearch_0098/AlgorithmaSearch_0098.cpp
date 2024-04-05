1ebac7350839f32dd1d784ecda13c7fc1b48d7b4
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-04-05 14:25:37 +0700

    masukkan void linearSearch

--- AlgorithmaSearch_0098/AlgorithmaSearch_0098.cpp
+++ AlgorithmaSearch_0098/AlgorithmaSearch_0098.cpp
@@ -17,7 +17,22 @@ void input()
 			cout << "\nArray should have minimum 1 and maximum 20 element.\n\n";
 
 	}
+
+	// Accept array element
+	cout << "\n-------------------\n";
+	cout << "Enter array element \n";
+	cout << "--------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
 	
+}
+
+void LinearSearch()
+{
+
 }
 
 
