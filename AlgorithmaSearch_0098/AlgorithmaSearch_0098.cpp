b283824d5a6b7d50aceee0c1c374f85b838e6b41
SYAFITO <Lenovo@DESKTOP-4CQ6R6H>
2024-04-05 14:11:37 +0700

    masukkan int arr

--- AlgorithmaSearch_0098/AlgorithmaSearch_0098.cpp
+++ AlgorithmaSearch_0098/AlgorithmaSearch_0098.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int arr[20]; // Array to be searched
+
+int n;      // Number of element in the array
+int i;      // Index of array element
 
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+
+
 
 
